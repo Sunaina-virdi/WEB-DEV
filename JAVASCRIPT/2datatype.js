4f9f05d80536718ed1dea6b017f63513a03623e1
Sunaina <122337517+Sunaina-virdi@users.noreply.github.com>
2024-04-02 14:21:50 +0530

    Add files via upload

---
+++ JAVASCRIPT/2datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code aas newer version
+
+//  alert("hello")
+
+console.log(3+3)
+console.log(3*'3');
+
+let name = "hitesh"
+let age =18
+let isLoggedIn = false
+console.log(typeof isLoggedIn);
+
+// number-> 2 to power 53
+// bigint  -> Number with more than 2^53
+// string ->""
+// boolean -> true/false
+// null    -> A special data type that represents nothing or undefined value
+// undefined -> Not declared variable
+// symbol ->  unique and immutable data types
+// object  -> {}
+// array   -> []
+// typeof ->which type of variable
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
