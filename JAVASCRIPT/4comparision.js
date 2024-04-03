c727683da2e80f82397bdac60851479f5642aa77
Sunaina <122337517+Sunaina-virdi@users.noreply.github.com>
2024-04-03 23:09:45 +0530

    Add files via upload

---
+++ JAVASCRIPT/4comparision.js
@@ -0,0 +1,24 @@
+// simple comparision
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2" > 1);
+console.log("02">1); //wrong
+
+// ess trah k conversion confusion m daal skte h
+console.log("null");
+console.log(null > 1);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("undefined"); //sabme false hi deta h
+console.log(undefined==0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+console.log("2" == 2);
+console.log("2" === 2); //false bcz string ko numeric
